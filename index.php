bfc0e19d930e730d92af0946e9b18b740c056898
LAMPEngineer <LAMP.Engineer@yahoo.com>
2020-04-12 17:53:15 +0530

    modified in listing

--- index.php
+++ index.php
@@ -2,28 +2,15 @@
 
 echo "<!DOCTYPE html>
 		<html>
-		<head>
-			<title></title>
-			<link rel=\"shortcut icon\" href=\"img/favicon.ico\" type=\"image/x-icon\" />
-		</head>
-		<body>";
+			<head>
+				<title></title>
+			</head>
+			<body>
+			 <h1>List Item</h1>
+						<ul>
 
+						    <li><a href='./item_module/'>Show Items</a></li>
 
-				
-			  	echo "<center>Gautam Prasad @ Digital Horizons</center>";
-
-
-			  	echo "<br/>";
-
-				echo " <h1>Item list API call:</h1>
-					<ul>
-
-						<li><a href='./item_module/data/'>JSON data</a></li>
-
-					    <li><a href='./item_module/'>Show Items</a></li>
-
-					</ul>";
-
-	echo "
-	</body>
-</html>";
+						</ul>
+			</body>
+		</html>";
